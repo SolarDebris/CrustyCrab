062876d790e74a7604ea809e5748c6183a1f0723
Robster4911 <robbieheine@gmail.com>
2022-10-12 16:04:08 -0400

    Modified Listener Class Implementation

--- src/bin/listener.rs
+++ src/bin/listener.rs
@@ -1,4 +1,4 @@
-use std::net::{SocketAddr, UdpSocket};
+use std::net::{SocketAddr, UdpSocket, TcpListner};
 
 // Send packets over
 //  - Bytes (UDP, TCP)
@@ -8,18 +8,42 @@ use std::net::{SocketAddr, UdpSocket};
 // Defines essential functions
 pub trait Listen {
     fn run();
+    fn run_udp();
+    fn run_tcp();
     fn new_connection(addr: SocketAddr);
 }
 
 // Defines a UDP listener
-pub struct UdpListener {
-    pub socket: UdpSocket,
+pub struct Listener {
+    pub udp_sock: UdpSocket,
+    pub tcp_sock: TcpListener,
+    pub id:u64,
+    pub status:u8,
     // some vector of sorts to store already made connections
 }
 
-//impl Listen for UdpListener {
-    // TODO
-//}
+impl Listen for Listener {
+
+    fn run(&self, protocol:u8, address:SocketAddr) {
+        match protocol {
+            0 => run_udp(self, address);
+            1 => run_tcp(self, address);
+        }
+    }
+
+    // listenes using raw UDP
+    fn run_udp(&self, address:SocketAddr) {
+        self.udp_sock = UdpSocket::bind(address).expect("couldn't bind to address");
+        loop {
+            
+        }
+    }
+
+    // listens using raw TCP
+    fn run_tcp(&self, address:SocketAddr) {
+
+    }
+}
 
 fn main(){
     let _port = 420;
