2df5cac295fccdbbaafb4882bbaa84afe22051f2
Alex Schmith <alexanderschmith@protonmail.com>
2022-10-14 20:47:50 -0400

    added listener object

--- src/bin/listener.rs
+++ src/bin/listener.rs
@@ -1,4 +1,4 @@
-use std::net::{SocketAddr, UdpSocket};
+use std::net::{SocketAddr, UdpSocket, TcpListener};
 use crabby_patty_formula::{Listener};
 
 // Send packets over
@@ -13,8 +13,12 @@ fn main(){
     let _protocol: &str = "http";
 
     let order = Listener {
-
-    }
+        udp_sock: ,
+        tcp_sock: ,
+        id: ,
+        status: ,
+        port: _port,
+    };
 
 
 }
