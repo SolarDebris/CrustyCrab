f5dc2d09803d22586478ec23931a22b1d4c73f4b
Robster4911 <robbieheine@gmail.com>
2022-12-02 09:39:37 -0500

    Module System Changes [+] Finished example module [+] Finished dispatch function and list_mods function in the usr_mods module
    TODO
    [!] Integrate module system with both listener and implant.

--- src/usr_mods.rs
+++ src/usr_mods.rs
@@ -1,12 +1,35 @@
+// Custom Module System
+// Description: A system that allows users to write their own custom
+//              post-exploit modules.
+
+
+/*** DEV TODO ***/
+// Add your module here in the format
+// mod module_name;
 mod example;
 
-pub fn dispatch(s: &str) {
-    match s {
+// takes in a &str (the name of the module), runs that module, and then
+// returns the output of the module as a vector of bytes.
+pub fn dispatch(s: &str) -> Vec<u8> {
+    let result = match s {
+
+        /*** DEV TODO ***/
+        // add a match statement for your module below in the format
+        // "module_name" => module_name::run(),
         "example" => example::run(),
         &_ => todo!(),
     };
+
+    return result.into_bytes();
 }
 
-pub fn list_mods() -> String {
-    "Not yet implemented.".to_string()
+
+// prints to the screen a list of all installed modules
+pub fn list_mods() {
+    println!("Installed Modules:");
+
+    /*** DEV TODO ***/
+    // add a print statement below for your module in the following format:
+    // println!("module_name - description of what the module does");
+    println!("example - a template for developers to write their own modules");
 }
--- src/usr_mods/example.rs
+++ src/usr_mods/example.rs
@@ -1,3 +1,37 @@
-pub fn run() {
+/* Example Module
+ * Author: Robbie Heine
+ * Description: This module acts as a template to demonstrate 
+ * 				how to write modules for the Crusty Crab.
+ * Supported Architectures: ALL
+ * Supported Operating Systems: ALL
+ */
 
+
+// All modules must have a run function that returns a String.
+// The returned String will be what is printed to the terminal
+// when run. If your module does not perform any collection or
+// has no need for printing anything, you can just return some
+// String indicating termination, say "Done" for example.
+
+// Run acts as a sort of 'main' for your module. It must always
+// take in no parameters, it must be public, and it must return
+// a String.
+
+pub fn run() -> String {
+	helper_func();
+
+	// Return value must be a String, not &str. Conversion can be
+	return String::from("Done");
+}
+
+
+
+// Any auxiliary functions you may want or need should NOT be
+// declared as public.
+fn helper_func() {
+	// this function just counts to 2^16
+	let mut i: u16 = 0;
+	while i < u16::MAX {
+		i += 1;
+	}
 }
